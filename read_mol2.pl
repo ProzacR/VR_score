c6856ddf8cb0bdfefd8a488e12cb7e7fde62b497
ProzacR <vytautas@BioInf-301.(none)>
2013-05-08 13:23:35 +0300

    ~finished mol2 parser

--- read_mol2.pl
+++ read_mol2.pl
@@ -34,7 +34,7 @@ while(!(@lines[$line_number] =~ /MOLECULE/)) {
 $line_number++;
 }
 $line_number++;
-#read MOLECULE part to STDERR now
+#read MOLECULE part (dump for now)
 while(!(@lines[$line_number] =~ /ATOM/)) {
 #print STDERR "molecule: ", @lines[$line_number];
 $line_number++;
@@ -45,7 +45,7 @@ $x=0;
 while(!(@lines[$line_number] =~ /BOND/)) {
 #print STDERR "atom: ", @lines[$line_number];
 #split lines into symbols
-push @{$atoms[$x]}, split(//, $lines[$line_number]);
+push @{$atoms[$x]}, split(/ +/, $lines[$line_number]);
 $x++;
 $line_number++;
 }
@@ -54,60 +54,47 @@ print Dumper \@atoms[0];
 #ignore BOND part for now....
 
 
-#make formated hash: FIXME
+#make formated hash:
 $x = 0;
 while($atoms[$x]) {
 #COLUMNS DATA TYPE CONTENTS
 #--------------------------------------------------------------------------------
-# 1 - 6 Record name "ATOM "
-$atom[$x]{'record'} = join "", @{$atoms[$x]}[0..5];
+# atom_id (integer) = the ID number of the atom at the time the file was
+# created. This is provided for reference only and is not used when the
+# .mol2
+# file is read into SYBYL.
+$atom[$x]{'atom_id'} = @{$atoms[$x]}[1];
 
-# 7 - 11 Integer Atom serial number.
-$atom[$x]{'number'} = join "", @{$atoms[$x]}[6..10];
+# atom_name (string) = the name of the atom.
+$atom[$x]{'atom_name'} = @{$atoms[$x]}[2];
 
-#13 - 16 Atom Atom name.
-$atom[$x]{'name'} = join "", @{$atoms[$x]}[12..15];
+#
+$atom[$x]{'x'} = @{$atoms[$x]}[3];
 
-#17 Character Alternate location indicator.
-$atom[$x]{'location'} = join "", @{$atoms[$x]}[16];
+#
+$atom[$x]{'y'} = @{$atoms[$x]}[4];
 
-#18 - 20 Residue name Residue name.
-$atom[$x]{'resname'} = join "", @{$atoms[$x]}[17..19];
+#
+$atom[$x]{'z'} = @{$atoms[$x]}[5];
 
-#22 Character Chain identifier.
-$atom[$x]{'chain'} = join "", @{$atoms[$x]}[21];
+# atom_type (string) = the SYBYL atom type for the atom
+$atom[$x]{'atom_type'} = @{$atoms[$x]}[6];
 
-#23 - 26 Integer Residue sequence number.
-$atom[$x]{'res_nr'} = join "", @{$atoms[$x]}[22..25];
+# subst_id (integer) = the ID number of the substructure containing the
+# atom.
+$atom[$x]{'subst_id'} = @{$atoms[$x]}[7];
 
-#27 AChar Code for insertion of residues.
-$atom[$x]{'ins_code'} = join "", @{$atoms[$x]}[26];
+# subst_name (string) = the name of the substructure containing the atom
+$atom[$x]{'subst_name'} = @{$atoms[$x]}[8];
 
-#31 - 38 Real(8.3) Orthogonal coordinates for X in Angstroms.
-$atom[$x]{'x'} = join "", @{$atoms[$x]}[30..37];
+# charge (real) = the charge associated with the atom
+$atom[$x]{'charge'} = @{$atoms[$x]}[9];
 
-#39 - 46 Real(8.3) Orthogonal coordinates for Y in Angstroms.
-$atom[$x]{'y'} = join "", @{$atoms[$x]}[38..45];
-
-#47 - 54 Real(8.3) Orthogonal coordinates for Z in Angstroms.
-$atom[$x]{'z'} = join "", @{$atoms[$x]}[46..53];
-
-#55 - 60 Real(6.2) Occupancy.
-$atom[$x]{'occupancy'} = join "", @{$atoms[$x]}[54..59];
-
-#61 - 66 Real(6.2) Temperature factor (Default = 0.0).
-$atom[$x]{'temp_factor'} = join "", @{$atoms[$x]}[60..65];
-
-#73 - 76 LString(4) Segment identifier, left-justified.
-$atom[$x]{'segment'} = join "", @{$atoms[$x]}[72..75];
-
-#77 - 78 LString(2) Element symbol, right-justified.
-$atom[$x]{'element'} = join "", @{$atoms[$x]}[76..77];
-
-#79 - 80 LString(2) Charge on the atom.
-$atom[$x]{'charge'} = join "", @{$atoms[$x]}[78..79];
+# status_bit (string) = the internal SYBYL status bits associated with the
+# atom
+$atom[$x]{'status_bit'} = @{$atoms[$x]}[10];
 
 $x++;
 }
 
-#print Dumper \@atom;
+print Dumper \@atom[0];
